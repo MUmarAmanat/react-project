4de0d29465659aac7b58b69302fbe6e19b78126f
umcr7 <umaramanat66@gmail.com>
2020-07-31 23:45:49 +0500

    +1. checkout Forn, +2. Checkout Form valdiateion, +3. Thanks page, +4. Routing

--- sportsstore/src/data/ActionCreators.js
+++ sportsstore/src/data/ActionCreators.js
@@ -1,7 +1,8 @@
-import { ActionTypes } from "./Types";
+import { ActionTypes, DataTypes } from "./Types";
 import { RestDataSource } from "./RestDataSource";
 
 const dataSource = new RestDataSource();
+
 export const loadData = (dataType, params) => ({
   type: ActionTypes.DATA_LOAD,
   payload: dataSource.GetData(dataType, params).then((response) => ({
@@ -11,11 +12,21 @@ export const loadData = (dataType, params) => ({
     params,
   })),
 });
+
 export const setPageSize = (newSize) => ({
   type: ActionTypes.DATA_SET_PAGESIZE,
   payload: newSize,
 });
+
 export const setSortProperty = (newProp) => ({
   type: ActionTypes.DATA_SET_SORT_PROPERTY,
   payload: newProp,
+});
+
+export const placeOrder = (order) => ({
+  type: ActionTypes.DATA_STORE,
+  payload: dataSource.StoreData(DataTypes.ORDERS, order).then((response) => ({
+    dataType: DataTypes.ORDERS,
+    data: response.data,
+  })),
 });
